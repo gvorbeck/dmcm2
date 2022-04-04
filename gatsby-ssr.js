e5a5b41657832bd7a5b94acbb1df29068381313e
Garrett Vorbeck <gvorbeck@gmail.com>
2022-04-04 00:53:07 -0400

    oh no

--- gatsby-ssr.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import React from 'react';
-import TopLayout from './src/components/TopLayout/TopLayout';
-
-export const wrapRootElement = ({ element }) => <TopLayout>{element}</TopLayout>;
