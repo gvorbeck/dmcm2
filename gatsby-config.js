21aad67cf0249e02b328cf0b47ea8953a49929b5
Garrett Vorbeck <gvorbeck@gmail.com>
2022-04-04 00:59:29 -0400

    start again

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Dungeon Master\'s Compaign Manager',
+    siteUrl: 'https://www.yourdomain.tld',
+  },
+  plugins: ['gatsby-plugin-image', 'gatsby-plugin-react-helmet', 'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'images',
+      path: './src/images/',
+    },
+    __key: 'images',
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'pages',
+      path: './src/pages/',
+    },
+    __key: 'pages',
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'content',
+      path: './src/content/',
+    },
+    __key: 'content',
+  },
+  {
+    resolve: 'gatsby-plugin-react-svg',
+    options: {
+      rule: {
+        include: /images/,
+      },
+    },
+  }],
+};
